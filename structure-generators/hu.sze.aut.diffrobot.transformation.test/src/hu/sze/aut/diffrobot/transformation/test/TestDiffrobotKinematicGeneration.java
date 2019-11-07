3761934250f96a1df2fc7646f6abf989a763e955
kyberszittya <kyberszittya@protonmail.ch>
2019-11-08 00:10:39 +0100

    Diff robot added

--- structure-generators/hu.sze.aut.diffrobot.transformation.test/src/hu/sze/aut/diffrobot/transformation/test/TestDiffrobotKinematicGeneration.java
+++ structure-generators/hu.sze.aut.diffrobot.transformation.test/src/hu/sze/aut/diffrobot/transformation/test/TestDiffrobotKinematicGeneration.java
@@ -2,8 +2,16 @@ package hu.sze.aut.diffrobot.transformation.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.StringWriter;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
@@ -22,9 +30,19 @@ class TestDiffrobotKinematicGeneration {
 		try {
 			doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
 			diffrobot_kinematic.generateKinematicDescription(doc, robot);
+			TransformerFactory tf = TransformerFactory.newInstance();
+		    Transformer transformer;
+		    transformer = tf.newTransformer();
+		    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+		    StringWriter writer = new StringWriter();
+		    transformer.transform(new DOMSource(doc), new StreamResult(System.out));
 		} catch (ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			fail(e.getMessage());
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 	}
